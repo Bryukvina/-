367a5f581f7284e8e22ca4c6fc02f48c3aee94e0
Bryukvina Darja <100697083+Bryukvina@users.noreply.github.com>
2022-09-02 22:05:11 +0300

    Create Change.cpp

---
+++ Change.cpp
@@ -0,0 +1,11 @@
+#include <iostream>;
+
+int main()
+{
+    int a, b;
+    cout >> "input a and b with space in between";
+    cin << a << b;
+    x = x + y; // x = 14 y = 5
+    y = x - y; // x = 14 y = 9
+    x = x - y;
+}
