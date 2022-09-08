096effa84835df0f4765ebb05e7de57ea7126e78
Bryukvina Darja <100697083+Bryukvina@users.noreply.github.com>
2022-09-08 18:01:06 +0300

    Create Change_system.cpp

---
+++ Change_system.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <map>
+#include <string>
+ 
+int main()
+{
+    map <string, double> constans = {{"Кл", 1.6E-19}, {"В", 3.3E3}, {"Дж", 1.6E-19}};
+    map <string, string> names = {{"Кл", "ед.заряда"}, {"В", "ед. потенциала"}, {"Дж", "эВ"}};
+
+    double x;
+    string constant;
+    string name;
+    
+    cout << "Type the value in SI system with space in between: ";
+    cin >> x >> constant;
+    
+    for (auto i : constans)
+    {
+        if (i.first == constant)
+            name == names.find(x);
+            cout << (x * i.second) << " " << name->second << endl;
+    }
+    
+    return 0;
+}
