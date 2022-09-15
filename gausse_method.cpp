7b53d36a5465c408814e600752069c3337209eb5
Bryukvina Darja <100697083+Bryukvina@users.noreply.github.com>
2022-09-15 17:52:14 +0300

    Update gausse_method.cpp

--- gausse_method.cpp
+++ gausse_method.cpp
@@ -6,64 +6,55 @@ int main()
     int i, j, k, n;
     double c1, c2; // const, that will be used in algoritm's ways
 
-    cout << "enter the size of the matrix: " << endl;
+    cout << "enter the count of equations: " << endl;
     cin >> n;
 
-    double **arr = new double *[n]; // creating of matrix with main constans
-    for (i = 0; i <= n; i++)
-        arr[i] = new double [n];
-
-    double **sup_arr = new double *[n]; // creating of support matrix
-    for (i = 0; i <= n; i++)
-        sup_arr[i] = new double [n];
-
-    double *b = new double [n]; // the list of the free mem
-    double *x = new double [n]; // list of the answers
+    double arr[n][n];
+    double b[n]; // the list of the free mem
+    double x[n]; // list of the answers
 
     cout << "write the matrix of factors" << endl;
 
-    for (i = 0; i <= n; i++) // reading the coeficients to the memory
+    for (i = 0; i < n; i++) // reading the coefficients to the memory
     {
-        for (j = 0; j <= n; j++)
+        for (j = 0; j < n; j++)
         {
             cin >> arr[i][j];
-            sup_arr[i][j] = arr[i][j];
         }
         cin >> b[i];
     }
 
     // first part of the algorithm
-    for (k = 0; k <= n; k++)
+    for (k = 0; k < n; k++)
     {
-        for (j = k; j <= n; j++)
+        for (j = k + 1; j < n; j++)
         {
-            c1 = arr[i][j] / arr[j][j];
-            for (i = k; i <= n; i++)
+            c1 = arr[j][k] / arr[k][k];
+            for (i = k; i < n; i++)
             {
-                arr[j][j] = arr[i][j] - c1 * arr[k][i];
+                arr[j][i] = arr[j][i] - c1 * arr[k][i];
             }
-            b[i] = b[i] - c1 * b[k];
+            b[j] = b[j] - c1 * b[k];
         }
     }
 
     // second part of the algorithm
-    for (k = n; k >= 0; k--)
+    for (k = n - 1; k >= 0; k--)
     {
-        c2 = 1;
-        for (j = k + 1; j <= n; j++)
+        c2 = 0;
+        for (j = k + 1; j < n; j++)
         {
             c1 = arr[k][j] * x[j];
             c2 = c2 + c1;
         }
         x[k] = (b[k] - c2) / arr[k][k];
-    }
-
+    };
+    
     //output the result
-    for (i = 0; i <= n; i++)
+    for (i = 0; i < n; i++)
     {
         cout << "x" << i << "= " << x[i] << endl;
     }
     
     return 0;
 }
-
