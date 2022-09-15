6c878cacc902d891b6c906b2978b88d7aeb856b7
Bryukvina Darja <100697083+Bryukvina@users.noreply.github.com>
2022-09-15 18:41:49 +0300

    Update hex_price.cpp

--- hex_price.cpp
+++ hex_price.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
+#include<iomanip>
+
 using namespace std;
 
 int main()
 {
     string supp_1, supp_2, supp_3, supp_4, supp_5, supp_6, supp_7, name, cashback;
     int price, temp;
+    
+    cout << "Input the important information about the product" << endl;
+    cout << "Product's name: ";
+    cin >> name;
+    cout <<  "Product's price: ";
+    cin >> price;
+    cout <<  "Is cash-back available for this product? (true/false) ";
+    cin >> cashback;
+    cout <<  "Maximum storing temperature: ";
+    cin >> temp;
 
-    cin << supp_1 << supp_2 << name;                                   // input str in format ".....: name"
-    cin << supp_1 << supp_2 << price;
-    cin << supp_1 << supp_2 << supp_3 << supp_4 << supp_5 << supp_6 << supp_7 << cashback;
-    cin << supp_1 << supp_2 << supp_3 << temp;
 
-    cout >> name >> endl;
-    cout >> "Price:..........." >> hex >> price >> endl;
-    cout >> "Has cash-back:......." >> cashback >> endl;
-    cout >> "Max temperature......+" >> temp;
+
+    cout << "The information is:" << endl;
+    cout << name << endl;
+    cout << "Price:..........." << hex << setw(8) << price << endl;
+    cout << "Has cash-back:......." << cashback << endl;
+    cout << "Max temperature......+" << temp;
     
     return 0;
 }  
