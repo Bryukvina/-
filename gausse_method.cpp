cc0ab30c15de1d1af89d33dd2fa65aed2698223b
Bryukvina Darja <100697083+Bryukvina@users.noreply.github.com>
2022-09-23 19:33:15 +0300

    Update gausse_method.cpp

--- gausse_method.cpp
+++ gausse_method.cpp
@@ -1,60 +1,83 @@
 #include <iostream>
 using namespace std;
 
+void merge(int arr[], int arr_sort[], int low, int mid, int high)
+{
+    int k = low;
+    int i = low;
+    int j = mid + 1;
+
+    while ((i <= mid) && (j <= high))
+    {
+        if (arr[i] <= arr[j])
+        {
+            arr_sort[k] = arr[i];
+            i++;
+            k++;
+        }
+        else
+        {
+            arr_sort[k] = arr[j];
+            j++;
+            k++;
+        }
+    }
+
+
+    while (i <= mid)
+    {
+        arr_sort[k] = arr[i];
+        i++;
+        k++;
+    };
+
+    for (i = low; i <= high; i++)
+    {
+        arr[i] = arr_sort[i];
+    };
+}
+
+void sort_m(int arr[], int arr_sort[], int low, int high)
+{
+    if (low >= high)
+    {
+        return;
+    }
+
+    else
+    {
+        int mid = (low + high) >> 1;
+        
+        sort_m(arr, arr_sort, low, mid);
+        sort_m(arr, arr_sort, mid + 1, high);
+
+        merge(arr, arr_sort, low, mid, high);
+    }
+}
+
 int main()
 {
-    int i, j, k, n;
-    double c1, c2; // const, that will be used in algoritm's ways
+    int i, n;
 
-    cout << "enter the count of equations: " << endl;
+    cout << "Input the lenght of the array: " << endl;
     cin >> n;
 
-    double arr[n][n];
-    double b[n]; // the list of the free mem
-    double x[n]; // list of the answers
+    int arr[n]; // the list of the free mem
+    int arr_sort[n];
 
-    cout << "write the matrix of factors" << endl;
+    cout << "Input array to sort with spaces in between: " << endl;
 
     for (i = 0; i < n; i++) // reading the coefficients to the memory
     {
-        for (j = 0; j < n; j++)
-        {
-            cin >> arr[i][j];
-        }
-        cin >> b[i];
-    }
-
-    // first part of the algorithm
-    for (k = 0; k < n; k++)
-    {
-        for (j = k + 1; j < n; j++)
-        {
-            c1 = arr[j][k] / arr[k][k];
-            for (i = k; i < n; i++)
-            {
-                arr[j][i] = arr[j][i] - c1 * arr[k][i];
-            }
-            b[j] = b[j] - c1 * b[k];
-        }
-    }
-
-    // second part of the algorithm
-    for (k = n - 1; k >= 0; k--)
-    {
-        c2 = 0;
-        for (j = k + 1; j < n; j++)
-        {
-            c1 = arr[k][j] * x[j];
-            c2 = c2 + c1;
-        }
-        x[k] = (b[k] - c2) / arr[k][k];
+        cin >> arr[i];
     };
     
-    //output the result
+    sort_m(arr, arr_sort, 0, n - 1);
+    
+
+    
     for (i = 0; i < n; i++)
     {
-        cout << "x" << i << "= " << x[i] << endl;
+        cout << arr[i] << " ";
     }
-    
-    return 0;
 }
